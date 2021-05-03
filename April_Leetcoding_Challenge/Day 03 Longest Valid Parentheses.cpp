b35cd75d961476a478ed5594a057219974ac0353
ParikshitPrnv <prnvgautam15@gmail.com>
2021-05-03 18:06:44 +0530

    routine update

---
+++ April_Leetcoding_Challenge/Day 03 Longest Valid Parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int>st;
+        int ans=0;
+        st.push(-1);
+        for(int i=0;s[i];i++){
+            if(s[i]=='(')
+                st.push(i);
+            else{
+                st.pop();
+                if(st.empty())
+                    st.push(i);
+                ans=max(ans,i-st.top());
+            }
+        }
+        return ans;
+    }
+};
